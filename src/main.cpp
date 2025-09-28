57bc95fa5d24b048e95ad1917caad4240e0d435f
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-09-28 09:07:57 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,19 @@
 using namespace geode::prelude;
 
 class $modify(GDPopupSoundsHook,FLAlertLayer) {
-    bool init(FLAlertLayer*self, gd::string title, gd::string message, gd::string button, gd::string button2, bool button2Visible, bool useSmallFont) {
-        if (! FLAlertLayer::init(self, title, message, button, button2, button2Visible, useSmallFont)) {
-            return false;
-        }
-
-        const std::string fullText = title + " " + message;
+    bool init(FLAlertLayerProtocol* delegate, char const* title, gd::string desc, char const* btn1, char const* btn2, float width, bool scroll, float height, float textScale) {
+        if (!FLAlertLayer::init(delegate, title, desc, btn1, btn2, width, scroll, height, textScale)) return false;
+        
+        const std::string fullText = std::string(title) + " " + desc;
 
         std::regex error_pattern("- \\d+");
 
         if (std::regex_search(fullText, error_pattern)) {
             FMODAudioEngine::sharedEngine()->playEffect("/rescources/GJ_Error.ogg"_spr);
-            // im fucking lazy shut up geode::audio::playSound("resources/GJ_Error.ogg"_spr);
-               } else {
-                FMODAudioEngine::sharedEngine()->playEffect("/resources/GJ_Popup.ogg"_spr);
-                // geode::audio::playSound("resources/GJ_Popup.ogg"_spr);
-               }
-    }
+        } else {
+            FMODAudioEngine::sharedEngine()->playEffect("/resources/GJ_Popup.ogg"_spr);
+        }
+
         return true;
     }
 };
